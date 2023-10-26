6c73ddb474a2cec804915c5cb4a2545ad4d8e4f3
Florence-nyokabi <florencenyokabiwangui@gmail.com>
2023-10-26 19:37:36 +0300

    Added the summary fragment

--- app/src/main/java/bills/project/billzapp/ui/MainActivity.kt
+++ app/src/main/java/bills/project/billzapp/ui/MainActivity.kt
@@ -15,7 +15,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        throw
     }
 
     override fun onResume() {
